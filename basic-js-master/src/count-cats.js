f3c5d452befbc6d69237df77eca9ad21990bddca
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-04-14 17:17:32 +0200

    feat: update

---
+++ basic-js-master/src/count-cats.js
@@ -0,0 +1,32 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix 
+ * @return {Number} count of cats found
+ *
+ * @example
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
+ *
+ */
+function countCats(backyard) {
+  throw new NotImplementedError('Not implemented');
+  let count = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === '^^') {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+module.exports = {
+  countCats
+};
