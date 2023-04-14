f00ad392b122305ce6f01a96dcd0b118edd07712
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-04-14 18:46:00 +0200

    feat: add function sampleActivity

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,9 +19,19 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(sampleActivity) {
   throw new NotImplementedError('Not implemented');
-  if (typeof sampleActivity !== "String" || isNaN(sampleActivity) || sampleActivity <= 0) {
-    return false
+  if (typeof sampleActivity !== "string") {
+    return false;
   }
+
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+
+  return t;
 }
 
 module.exports = {
