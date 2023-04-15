adc70dbf38a1ee125ce2789aafcae5735fa71d64
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-04-16 00:46:25 +0200

    fix: add instanceof

--- src/what-season.js
+++ src/what-season.js
@@ -14,14 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
-  } else if (date.getMonth() === 0 || date.getMonth() === 11 || date.getMonth() === 1 || date.getMonth() === 'winter') {
+  }
+
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+  }
+  //if (date === !Object.getOwnPropertyNames(Date.prototype)) {
+  // throw new Error('Invalid date!');
+  //}
+  if (date.getMonth() === 0 || date.getMonth() === 11 || date.getMonth() === 1) {
     return 'winter';
-  } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4 || date.getMonth() === 'spring') {
+  } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
     return 'spring';
-  } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7 || date.getMonth() === 'summer') {
+  } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
     return 'summer';
-  } else {
-    return /autumn|fall/;
+  } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+    return "autumn";
   }
 }
 
